1956a64c72be0156e5855f935c0147a49dd95951
Chris Farrell <mrchrisfarrell@yahoo.com>
2025-11-30 08:47:58 -0500

    added routes and details. commented app.html code from project init. Houses show as grid now. Logo won't load despite seeming to be referenced correctly

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication, provideProtractorTestingSupport } from '@angular/platform-browser';
+import {provideRouter} from '@angular/router';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import routeConfig from './app/routes';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [provideProtractorTestingSupport(), provideRouter(routeConfig)],
+}).catch((err) => console.error(err));
